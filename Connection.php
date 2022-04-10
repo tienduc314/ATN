1282ed673964d48cc4de796c1125a06d3166e60e
tienduc314 <91647974+tienduc314@users.noreply.github.com>
2022-04-11 04:19:31 +0700

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -2,4 +2,3 @@
     $conn = pg_connect("postgres://vpdrjcqrwvvcqy:344c8737a25b07158aa9503ea81b016ed9272f6a7c415c9bbea3e6f837c111ad@ec2-34-194-158-176.compute-1.amazonaws.com:5432/dc3pj98koi2n5u")
         or die("Cannot connect database".pg_connect_error());
 ?>
-
