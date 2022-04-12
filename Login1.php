9a3ed935eaedb1d03f23245285567b95d45553ef
tienduc314 <91647974+tienduc314@users.noreply.github.com>
2022-04-12 16:57:01 +0700

    Update Login1.php

--- Login1.php
+++ Login1.php
@@ -47,28 +47,27 @@
 
 <h1>Login</h1>
 <form id="form1" name="form1" method="POST">
-<div class="row">
-    <div class="form-group">				    
-        <label for="txtUsername" class="col-sm-2 control-label">Username(*):</label>
-		<div class="col-sm-10">
-		      <input type="text" name="txtUsername" id="txtUsername" class="form-control" placeholder="Username" value=""/>
-		</div>
-      </div>  
-      
-    <div class="form-group">
-		<label for="txtPass" class="col-sm-2 control-label">Password(*):</label>			
-		<div class="col-sm-10">
-		      	<input type="password" name="txtPass" id="txtPass" class="form-control" placeholder="Password" value=""/>
-		</div>
-	</div> 
-	<div class="form-group"> 
-        <div class="col-sm-2"></div>
-        <div class="col-sm-10">
-        	<input type="submit" name="btnLogin"  class="btn btn-primary" id="btnLogin" value="Login"/>
-            <input type="submit" name="btnCancel"  class="btn btn-primary" id="btnLogin" value="Cancel"/>
-		</div>  
-	</div>
- </div>
-  
+    <div class="row">
+        <div class="form-group">				    
+            <label for="txtUsername" class="col-sm-2 control-label">Username(*):</label>
+            <div class="col-sm-10">
+                <input type="text" name="txtUsername" id="txtUsername" class="form-control" placeholder="Username" value=""/>
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label for="txtPass" class="col-sm-2 control-label">Password(*):</label>			
+            <div class="col-sm-10">
+                    <input type="password" name="txtPass" id="txtPass" class="form-control" placeholder="Password" value=""/>
+            </div>
+        </div>
 
+        <div class="form-group"> 
+            <div class="col-sm-2"></div>
+            <div class="col-sm-10">
+                <input type="submit" name="btnLogin"  class="btn btn-primary" id="btnLogin" value="Login"/>
+                <input type="submit" name="btnCancel"  class="btn btn-primary" id="btnLogin" value="Cancel"/>
+            </div>  
+        </div>
+    </div>
 </form>
