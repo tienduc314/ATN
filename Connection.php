3263f5c656dd2c6c07c799ba512047d2b4a44d78
tienduc314 <91647974+tienduc314@users.noreply.github.com>
2022-05-05 16:11:38 +0700

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -1,4 +1,4 @@
 <?php
-    $conn = pg_connect("postgres://gpfcgsidrkqkfh:2563ab31a7befea3318efad14c18993be392aaf890c66ad30c79abaf426d6970@ec2-44-194-4-127.compute-1.amazonaws.com:5432/d3dvm2f7qmgu8b")
+    $conn = pg_connect("postgres://ruspbqtdbhkvfg:0294668303e797292a55db208f98e3638be77f9fe23dd7e964024b74d84029e1@ec2-44-194-4-127.compute-1.amazonaws.com:5432/ddcs19b34185hb")
         or die("Cannot connect database".pg_connect_error());
 ?>
